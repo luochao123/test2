3efdb49fbfc3c473ce2a20fa36a611b3f5143f7f
camel4197 <luo_chao4197@126.com>
2020-02-01 15:55:47 +0800

    Initial Commit

---
+++ test2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test2
+//
+//  Created by 骆超 on 2020/2/1.
+//  Copyright © 2020 luochao.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
